529bda2847dd510a702f86b6d022031ce4625b30
masthi <masthan.rao@gmail.com>
2017-09-10 22:48:26 +0530

    Hello World Sample for plain Send & Receive

---
+++ RabbitMQHelloWorld/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQHelloWorld
+{
+
+    class Send
+    {
+        public static void Main()
+        {
+            string url = "amqp://cgsbbkwy:_Pxh9bx2ZlnDaIE34O1oJuD4UjmDV2W3@rhino.rmq.cloudamqp.com/cgsbbkwy";
+
+            var factory = new ConnectionFactory() { HostName = "rhino.rmq.cloudamqp.com" };
+            factory.Uri = new System.Uri(url.Replace("amqp://", "amqps://"));
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    string message = "Hello Masthan";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "Hello",
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine("[x] sent {0}", message);
+                }
+            }
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+   
+}
---
+++ Receive/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            string url = "amqp://cgsbbkwy:_Pxh9bx2ZlnDaIE34O1oJuD4UjmDV2W3@rhino.rmq.cloudamqp.com/cgsbbkwy";
+
+            var factory = new ConnectionFactory() { HostName = "rhino.rmq.cloudamqp.com" };
+            factory.Uri = new System.Uri(url.Replace("amqp://", "amqps://"));
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "Hello",
+                        autoAck: true,
+                        consumer: consumer);
+                    Console.WriteLine("Press [Enter] to exit");
+                    Console.ReadLine();
+                }
+
+            }
+
+        }
+    }
+}
